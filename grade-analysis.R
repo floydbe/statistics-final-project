6a3ed862e2abdb083252f56365467fc124ea98d2
floydbe <bef2cj@virginia.edu>
2015-12-14 19:49:44 -0500

    final(?) touches to grade-analysis

--- grade-analysis.R
+++ grade-analysis.R
@@ -1,18 +1,38 @@
-male <- read.csv(paste(getwd(),"/male.csv", sep=""), header=TRUE)
-female <- read.csv(paste(getwd(), "/female.csv", sep=""), header=TRUE)
-unknown <- read.csv(paste(getwd(), "/unknown.csv", sep=""), header=TRUE)
+grade_data <- read.csv(paste(getwd(),"/grades.csv", sep=""), header=TRUE)
+males <- subset(grade_data, grade_data$gender=="male")
+females <- subset(grade_data, grade_data$gender=="female")
+unknown <- subset(grade_data, grade_data$gender=="unknown")
 
-m <- male$grades
-f <- female$grades
-u <- unknown$grades
+# descriptive statistics
+aggregate(grade_data$grade, by=list(grade_data$gender), mean)
+aggregate(grade_data$grade, by=list(grade_data$gender), sd)
 
-mean(m)
-sd(m)
+# side-by-side boxplot for grade data
+png("boxplot.png")
+boxplot(grade~gender,data=grade_data)
+dev.off()
 
-mean(f)
-sd(f)
+# perform an ANOVA
+g_model <- aov(grade~gender, data=grade_data)
+summary(g_model)
+# ANOVA isn't significant, but do Tukey's just to see it
+TukeyHSD(g_model)
+# define omega squared effect size function
+omega_sq <- function(aovm){
+	sum_stats <- summary(aovm)[[1]]
+	SSm <- sum_stats[["Sum Sq"]][1]
+	SSr <- sum_stats[["Sum Sq"]][2]
+	DFm <- sum_stats[["Df"]][1]
+	MSr <- sum_stats[["Mean Sq"]][2]
+	W2 <- (SSm-DFm*MSr)/(SSm+SSr+MSr)
+	return(W2)
+}
+# use it to compute omega squared
+omega_sq(g_model)
 
-mean(u)
-sd(u)
+# test only male and female with independent samples t-test
+t.test(males$grade,females$grade)
 
-t.test(m,f)
+# effect size for the t-test
+source("cohensD.R")
+cohensD(males$grade, females$grade)
